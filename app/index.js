788bdc0f84d0d880c2e34931ba9d1e0367af5a89
playwolsey <chengpengcp9@163.com>
2018-05-23 23:00:17 +0800

    404 added

--- app/index.js
+++ app/index.js
@@ -9,6 +9,27 @@ app.use(async function (ctx) {
   await ctx.render('./views/index.html')
 });
 
+app.use(async function pageNotFound(ctx) {
+  // we need to explicitly set 404 here
+  // so that koa doesn't assign 200 on body=
+  ctx.status = 404;
+
+  switch (ctx.accepts('html', 'json')) {
+    case 'html':
+      ctx.type = 'html';
+      ctx.body = '<p>Page Not Found</p>';
+      break;
+    case 'json':
+      ctx.body = {
+        message: 'Page Not Found'
+      };
+      break;
+    default:
+      ctx.type = 'text';
+      ctx.body = 'Page Not Found';
+  }
+});
+
 app.listen(3000, () => {
   console.log('koa server running on http://localhost:3000');
 });
